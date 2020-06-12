c0883a5c72c631f4320adb1462f5ac7d7157b052
diego64 <diegoferreira1964@gmail.com>
2020-06-12 17:31:11 -0300

    Novas funcionalidades e modifição na estruta de alumas páginas

--- src/server.js
+++ src/server.js
@@ -1,43 +1,98 @@
-const express = require("express");
-const server = express();
+const express = require("express")
+const server = express()
 
-//Banco de daddos
-const db = require("./database/db");
+//Conexão com o BD
+const db = require("./database/db")
 
-//Configuração das Pastas Publicas
-server.use(express.static("public"));
+//Configuração do acesso a pasta Publica
+server.use(express.static("public"))
 
-const nunjucks = require("nunjucks");
+//Habilitação para o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true }))
+
+
+//Template Engine
+const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
     noCache: true
-});
+})
 
-//Paginia Inicial
+//Rota Principal (Home)
 server.get("/", (req, res) => {
-    res.render("index.html", { title: "Seu marketplace de coletas de resíduos"})
-});
+    return res.render("index.html", { title: "Um título"})
+})
 
-//Pagina de cadastro
+//Cadastro 
 server.get("/create-point", (req, res) => {
-    console.log(req.query)
+    // req.query: Query Strings da nossa url
+    // console.log(req.query)
+    return res.render("create-point.html")
+})
 
+//Insert de dados
+server.post("/savepoint", (req, res) => {
+    // Inserindo dos dados do Formulario no BD
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
 
-   return res.render("create-point.html")
-});
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
 
-//Página de procura e resultado de procura
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
+//Busca
 server.get("/search", (req, res) => {
-    db.all(`SELECT * FROM places`, function(err, rows) {
+
+    const search = req.query.search
+
+    if(search == "") {
+        //Pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
+    // Buscando as cidades no banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
 
-        const total = rows.length;
+        const total = rows.length
 
-        res.render("search-results.html", {places: rows, total: total})
-    });
-});
+        //Total de itens na busca
+        return res.render("search-results.html", { places: rows, total: total})
+    })
+})
 
-//Start do Servidor 
-server.listen(3000);
+//Start ON no servidor
+server.listen(3000)
